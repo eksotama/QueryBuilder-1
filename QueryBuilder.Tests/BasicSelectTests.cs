6f5e9400f192ef95123796fa2a60e9f4375dded0
Jalal <sjamini@hotmail.com>
2014-12-10 01:03:54 +0330

    some cleanups

---
+++ QueryBuilder.Tests/BasicSelectTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QueryBuilder.Tests
+{
+    [TestClass]
+    public class BasicSelectTests
+    {
+        [TestMethod]
+        public void CeateBasicSelectStatement()
+        {
+            const string expected = "SELECT Id  FROM People";
+
+            var queryBuilder = new SelectQueryBuilder();
+            queryBuilder.SelectColumn("Id");
+            queryBuilder.SelectFromTable("People");
+
+            var result = queryBuilder.BuildQuery();
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
--- QueryBuilder/Sorting.cs
+++ QueryBuilder/Sorting.cs
@@ -1,12 +1,3 @@
-
-//
-// Enum: Sorting
-// Copyright 2006 by Ewout Stortenbeker
-// Email: 4ewout@gmail.com
-//
-// This enum is part of the CodeEngine Framework.
-// You can download the framework DLL at http://www.code-engine.com/
-//
 namespace QueryBuilder
 {
     /// <summary>
